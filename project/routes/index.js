d72891216fd0991cd6aefdea290547fa04e4eaf7
Kimyeongho <dudgh9661@naver.com>
2020-06-11 21:38:55 +0900

    original project

---
+++ project/routes/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+// Main Page
+router.get('/about', (req, res) => {
+    res.render('../views/about.html');
+});
+
+router.get('/info', (req, res) => {
+    res.render('../views/info.html');
+});
+
+router.get('/deposit', (req, res) => {
+    res.render('../views/deposit.html');
+});
+
+router.get('/withdraw', (req, res) => {
+    res.render('../views/withdraw.html');
+});
+
+router.get('/storeAdd', (req, res) => {
+    res.render('../views/storeAdd.html');
+});
+
+router.get('/store', (req, res) => {
+    res.render('../views/store.html');
+});
+
+router.get('/customerAdd', (req, res) => {
+    res.render('../views/customerAdd.html');
+});
+
+
+module.exports = router;
